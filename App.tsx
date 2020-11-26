84ebb80786b9b473593e31d551e8c713a8a77d13
Emilio Schepis <emilio.schepis@gmail.com>
2020-11-26 19:42:04 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <></>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default App;
