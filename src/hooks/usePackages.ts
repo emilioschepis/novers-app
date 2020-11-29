22214d2c68cecf0c02c2531b8e15e938578ac1e7
Emilio Schepis <emilio.schepis@gmail.com>
2020-11-29 15:40:17 +0100

    feat: add simple cache implementation

--- src/hooks/usePackages.ts
+++ src/hooks/usePackages.ts
@@ -7,14 +7,35 @@ export default (names: string[]) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<Package[] | undefined>();
   const [error, setError] = useState<any | undefined>();
+  const [cache, setCache] = useState<Package[]>([]);
 
   useEffect(() => {
+    if (names.length === 0) {
+      return;
+    }
+
+    let localNames = new Set(names);
+    let localPackages = new Set<Package>([]);
+    for (const cachedPackage of cache) {
+      if (localNames.has(cachedPackage.name)) {
+        localPackages.add(cachedPackage);
+        localNames.delete(cachedPackage.name);
+      }
+    }
+
+    if (localNames.size === 0) {
+      // We already have information about all packages
+      setData(Array.from(localPackages));
+      return;
+    }
+
     setLoading(true);
 
     api
-      .fetchPackages(names)
+      .fetchPackages(Array.from(localNames))
       .then((packages) => {
-        setData(packages);
+        setCache(current => current.concat(packages));
+        setData(Array.from(localPackages).concat(packages));
       })
       .catch((error) => {
         setError(error);
--- src/screens/home/components/PackageList.tsx
+++ src/screens/home/components/PackageList.tsx
@@ -9,25 +9,6 @@ const PackageList: React.FC = () => {
   const { names, removeName } = useContext(PackagesContext);
   const { loading, error, data } = usePackages(names);
 
-  useEffect(() => {
-    if (data && data.length > 0 && names.length > 0) {
-      if (data.length === names.length) {
-        // Short-circuit because there is no mismatch between packages and names.
-        return;
-      }
-
-      const dataNames = data.map((element) => element.name);
-
-      // Remove all elements that are not returned in the request.
-      // TODO: Update this when the API supports returning an error for non-existent names.
-      names
-        .filter((name) => !dataNames.includes(name))
-        .forEach((name) => {
-          removeName(name);
-        });
-    }
-  }, [names, data]);
-
   if (error) {
     return (
       <View style={styles.centerContainer}>
